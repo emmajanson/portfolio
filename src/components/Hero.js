dd772bcfb212aaa220390c15ea860fa98b3db623
emma3dwn <emma.j@3dwn.se>
2023-02-08 14:33:12 +0100

    update hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from '../styles/Hero.module.css'
 import Image from 'next/image'
-import '../../public/heroImg.png'
+import '../../public/heroImg.jpg'
 import { motion as m } from "framer-motion"
   
 function Hero() {
   return (
     <section className={styles.heroSection}>
-          <img className={styles.heroImg} src="heroImg.png" alt="Hero Image"></img>
+          <img className={styles.heroImg} src="heroImg.jpg" alt="Hero Image"></img>
     </section>
   )
 }
